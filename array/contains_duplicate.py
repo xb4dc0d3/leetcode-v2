e05234b3ca4be3c6611efe9d4a94c81d55419520
xb4dc0d3 <johnbakeryholland@gmail.com>
2024-02-03 13:36:43 +0700

    feat: [Array] Contains Duplicate

---
+++ array/contains_duplicate.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for idx in range(len(nums)-1):
+            if nums[idx] == nums[idx+1]:
+                return True
+        return False
