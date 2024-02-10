9448df4c05eafac07e851f8f46d1c5046cfff955
xb4dc0d3 <johnbakeryholland@gmail.com>
2024-02-10 20:45:27 +0700

    feat(sliding-window): add permutationg string

--- stack/asteroid_collections.py
+++ stack/asteroid_collections.py
@@ -1,3 +1,21 @@
+from typing import List
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        pass
+        stack = []
+        for i in range(len(asteroids)):
+            while stack and asteroids[i]<0<stack[-1]:    
+                if -asteroids[i]>stack[-1]:
+                    stack.pop()
+                    continue
+                elif -asteroids[i]==stack[-1]:
+                    stack.pop()
+                break
+            else:
+                stack.append(asteroids[i])
+        return stack
+
+
+
+# s = Solution()
+# asteroids = [5,10,-20]
+# print(s.asteroidCollision(asteroids))
